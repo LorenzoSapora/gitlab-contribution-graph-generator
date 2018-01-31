091f0b5562d4d18d539130682e374eacc797d37e
Lorenzo Sapora <lorenzo.sapora@gmail.com>
2018-01-31 14:49:34 +0000

    Add requests headers and params

--- main.py
+++ main.py
@@ -9,3 +9,16 @@ for d in reversed(range(365)):
     date_after = (now - timedelta(days=(d + 1))).strftime("%Y-%m-%d")
     date_current = (now - timedelta(days=d)).strftime("%Y-%m-%d")
     date_before = (now - timedelta(days=(d - 1))).strftime("%Y-%m-%d")
+
+
+    headers = {
+        'PRIVATE-TOKEN': config.privatetoken,
+    }
+
+    params = (
+        ('before',date_before),
+        ('after',date_after),
+        ('per_page', '99'),
+    )
+
+    response = requests.get(config.gitlaburl,headers=headers,params=params)

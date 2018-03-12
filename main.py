287cf2ce1f0c3e493b8a6f7190687323349cc9ab
Lorenzo Sapora <lorenzo.sapora@gmail.com>
2018-03-12 06:53:45 +0000

    Fix range

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ open(output, 'w')
 fcsv = open(output, 'a+')
 print >> fcsv, "date,events"
 
-for d in reversed(range(365)):
+for d in reversed(range(364)):
     date_after = (now - timedelta(days=(d + 1))).strftime("%Y-%m-%d")
     date_current = (now - timedelta(days=d)).strftime("%Y-%m-%d")
     date_before = (now - timedelta(days=(d - 1))).strftime("%Y-%m-%d")
